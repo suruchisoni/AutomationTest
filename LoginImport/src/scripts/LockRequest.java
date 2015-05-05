7081b614443f9895385e91f65037e38595a55d95
suruchisoni <suruchisoni@gmail.com>
2015-05-04 23:43:47 -0700

    Login import

--- LoginImport/src/scripts/LockRequest.java
+++ LoginImport/src/scripts/LockRequest.java
@@ -1,26 +1,53 @@
 package scripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import resusableClass.Import;
 import resusableClass.WebPage;
 
-public class LockRequest {
-    //creating reference of 2 classes
-	private Import importObject;
-	private WebPage webPage;
-
-	public void beforeTest() {
-		//creating new objects
-		importObject = new Import();
-		webPage = new WebPage();
-		
-		webPage.openBrowser();
-		
-		
-		//importObject.setDriver(webPage.openBrowser());
+public class LockRequest{
+	WebPage webpageobj;
+	Import importobj;
+	FirefoxDriver driver;
+	
+	@BeforeTest
+	public void beforeTest() throws InterruptedException{
+		webpageobj = new WebPage();
+		importobj = new Import();
+		driver = webpageobj.openBrowser();
+		importobj.setDriver(driver);
+		importobj.loginTest();
+		importobj.importData();
 	}
+		
+	/*@AfterTest
+	public void afterTest(){
+		webpageobj.closeBrowser();		
+	}*/
+		
+	@Test
+	public void lockRequest() throws InterruptedException{
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(".//*[@id='secMenu5']/a/span")).click();
+		
+		Thread.sleep(5000);
  
-	public void afterTest() {
-		webPage.closeBrowser();
+		Select lockPeriod = new Select(driver.findElement(By.id("LockPeriodCD")));
+		lockPeriod.selectByVisibleText("30");
+		
+		
+		
+		
+		
+		
 	}
-
+	
+	
+	
+	
 }
